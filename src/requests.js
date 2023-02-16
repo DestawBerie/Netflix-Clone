7c68e62cd5d5784ebfbc48a6be7abd3085e90a59
DestawBerie <102520194+DestawBerie@users.noreply.github.com>
2023-02-16 10:02:50 +0300

    netflix clone

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,6 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
-// const API_KEY = ac809092eaeb22fd51856116e9ea54d7;
+const API_KEY = "ac809092eaeb22fd51856116e9ea54d7";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
